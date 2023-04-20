581f8109ad7d14fa5ca103062ccd13ce70b82e6b
engkamakia <georgekamakia@gmail.com>
2023-04-20 14:06:40 +0300

    corrections

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -1,5 +1,5 @@
 # include "variadic_functions.h"
-# include <stdarg.h>
+
 
 /**
  * sum_them_all -function that returns the sum of all its parameters
@@ -44,4 +44,3 @@ int sum_them_all(const unsigned int n, ...)
     return (sum);
 
 }
-
