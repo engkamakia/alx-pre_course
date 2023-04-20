e070083fcc5bd8c455538100a9dc8104bbfcc84e
engkamakia <georgekamakia@gmail.com>
2023-04-20 13:56:37 +0300

    corrections

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -26,11 +26,11 @@ int sum_them_all(const unsigned int n, ...)
      *
      */
 
-    if(n == 0)
-        return(0);
+    if (n == 0)
+    return (0);
 
-    va_start(ptr,n);
-    for (i=0;i<n;i++)
+    va_start(ptr, n);
+    for (i = 0; i < n; i++)
 
     /**
      * accessing current variable
