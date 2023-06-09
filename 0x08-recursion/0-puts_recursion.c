a481a8afeb6d2f89ff096187593717e32100ead0
engkamakia <georgekamakia@gmail.com>
2023-06-09 12:08:59 +0300

    betty style corrections

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -4,15 +4,15 @@
  * _puts_recursion - prints a string, followed by a new line
  *
  *_putchar - writes the character c to stdout
- *
+ * *s - string to be passed to the _puts_recursion function
  */
 void _puts_recursion(char *s)
 {
-    if (*s)
-    {
-      _putchar(*s);
-      _puts_recursion(s + 1);
-    }
-    else
-      _putchar('\n');
+if (*s)
+{
+_putchar(*s);
+_puts_recursion(s + 1);
+}
+else
+_putchar('\n');
 }
