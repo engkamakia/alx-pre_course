4eb7b99ba6f51ee985491a1ea7eecc7807ae9e58
engkamakia <georgekamakia@gmail.com>
2023-06-09 15:51:41 +0300

    corrections on betty declaring parameters

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -3,6 +3,7 @@
 /**
  * _print_rev_recursion - function to print a string in reverse
  * _putchar - writes the character c to stdout
+ * 
  * @s:string to be passed to the _puts_recursion function
  *
  */
