06e9b8b76096885a644c14d1166e46e1e9cdfe76
engkamakia <georgekamakia@gmail.com>
2023-06-09 12:03:23 +0300

    code for She locked away a secret, deep inside herself, something she once knew to be true... but chose to forget

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,13 +1,18 @@
-#include <unistd.h>
+#include "main.h"
 
 /**
- * _putchar - writes the character c to stdout
- * @c: The character to print
+ * _puts_recursion - prints a string, followed by a new line
  *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
+ *_putchar - writes the character c to stdout
+ *
  */
-int _putchar(char c)
+void _puts_recursion(char *s)
 {
-	return (write(1, &c, 1));
+    if (*s)
+    {
+      _putchar(*s);
+      _puts_recursion(s + 1);
+    }
+    else
+      _putchar('\n');
 }
