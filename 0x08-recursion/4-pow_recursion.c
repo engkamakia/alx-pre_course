7d170d92e0a383213498a3030ccfcf0c2a0aa926
engkamakia <georgekamakia@gmail.com>
2023-06-09 15:54:29 +0300

    corrections on betty declaring parameters

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -3,7 +3,7 @@
  * _pow_recursion - function to returns the value of x raised to the power of y
  * @x:number to be used as base.
  * @y:the number of times the base number is to multiply itself.
- * Return: returns value of x raised to power y.  
+ * Return: returns value of x raised to power y. 
  */
 int _pow_recursion(int x, int y)
 {
