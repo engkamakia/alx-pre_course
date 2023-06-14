325c4bb4f2153d6ec1dc3c6cad7c4b5e05e96418
engkamakia <georgekamakia@gmail.com>
2023-06-14 16:05:35 +0300

    corrections on return value

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -9,36 +9,38 @@
  */
 int **alloc_grid(int width, int height)
 {
+int a = 0;
+int b;
+int **grid;
 
-	int a = 0, b;
-	int **grid;
-
-	if (width <= 0 || height <= 0)
-	{
-		return (NULL);
-	}
-	grid = malloc(height * sizeof(int *));
-
-	for (a = 0; a < height; a++)
-	{
-		grid[a] = malloc(width * sizeof(int));
-		if (!grid[a])
-		{
-			for (b = 0; b <= a; b++)
-			{
-				free(grid[b]);
-			}
-			free(grid);
-			return (NULL);
-		}
-	}
-	for (a = 0; a < height; a++)
-	{
-		for (b = 0; b < width; b++)
-		{
-			grid[a][b] = 0;
-		}
-	}
-	return (grid);
+if (width <= 0 || height <= 0)
+{
+return (NULL);
 }
-
+grid = malloc(sizeof(int *) * height);
+if (!grid)
+{
+return (NULL);
+}
+for (a = 0; a < height; a++)
+{
+grid[a] = malloc(width * sizeof(int));
+if (!grid[a])
+{
+for (b = 0; b <= a; b++)
+{
+free(grid[b]);
+}
+free(grid);
+return (NULL);
+}
+}
+for (a = 0; a < height; a++)
+{
+for (b = 0; b < width; b++)
+{
+grid[a][b] = 0;
+}
+}
+return (grid);
+}
