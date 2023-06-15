555a12afdc92d905227e0a7ac0661d4f7070d653
engkamakia <georgekamakia@gmail.com>
2023-06-15 13:37:01 +0300

    added header file

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
+#include "main.h"
+
 /**
  * main - function that takes cmdline args.
  * @argc: no of args.
  * @argv: array of args:
  * Return: returns(0) success.
  */
-int main(int argc, char  __attribute__((unused)) *argv[])
+int main(int argc, char __attribute__((unused)) *argv[])
 {
-printf("%d\n", argc -1);
+printf("%d\n", argc - 1);
 return (0);
 }
