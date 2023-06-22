7b2982bfd6ff4f317e7c5940472627ba5e2c5ff3
engkamakia <georgekamakia@gmail.com>
2023-06-22 10:45:14 +0300

    2. File name

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ * main - prints name of the file it was compiled followed by a new line.
+ * Return: (0) success.
+ */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
