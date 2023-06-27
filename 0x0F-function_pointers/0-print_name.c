a5cbbfd3f251299cd3fb7dd34cbfe835b0884635
engkamakia <georgekamakia@gmail.com>
2023-06-27 10:45:10 +0300

    corrections

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -3,12 +3,12 @@
 /**
  * print_name - function that prints a name.
  * @name: name the function takes as argument.
- * @ptr_func: function pointer.
+ * @f: function pointer.
  *
  * Return: returns nothing.
  */
-void print_name(char *name, void (*ptr_func)(char *))
+void print_name(char *name, void (*f)(char *))
 {
-	if (name && ptr_func)
-		ptr_func(name);
+if (name && f)
+f(name);
 }
