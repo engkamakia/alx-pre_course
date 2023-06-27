068bfdcdcc4db0e33d4bfb4231ba255895b9d512
engkamakia <georgekamakia@gmail.com>
2023-06-27 14:53:44 +0300

    corrections

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -2,7 +2,7 @@
 
 /**
  * print_name - function that prints a name.
- * @name: name the function takes as argument.
+ * @name: name that the function takes as argument.
  * @f: function pointer.
  *
  * Return: returns nothing.
