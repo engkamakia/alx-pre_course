d5e98a6076bf3ae95660555b89fa7cce7d162b4c
engkamakia <georgekamakia@gmail.com>
2023-07-05 11:47:35 +0300

    1-main.c file

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,10 +1,10 @@
 #include "lists.h"
 #include <stdio.h>
 /**
- * print_listint - a fucntion that prints elements of a node
- * @h: a pointer to the structure list_int_s
+ * print_listint - a function that prints no of nodes.
+ * @h: a pointer to the structure list_int_s.
  *
- * Return: length of the list
+ * Return: length of the list.
  */
 size_t print_listint(const listint_t *h)
 {
